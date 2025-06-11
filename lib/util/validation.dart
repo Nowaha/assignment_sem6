01973980dfc05294afd467bfd02c48ebf006cd9f
Nowaha <nowaha@proton.me>
2025-06-11 15:50:46 +0200

    Add (dummy) name / password validation

--- lib/util/validation.dart
+++ lib/util/validation.dart
@@ -1,4 +1,6 @@
 class Validation {
+  static bool isValidName(String name) => true;
   static bool isValidEmail(String email) => true;
+  static bool isValidPassword(String password) => true;
   static bool isValidUsername(String username) => true;
 }
