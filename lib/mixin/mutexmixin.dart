64b1b49ee58eaee7da1312169260af8d1ea44b3e
Nowaha <nowaha@proton.me>
2025-06-17 16:12:48 +0200

    Reformat MutexMixin

--- lib/mixin/mutexmixin.dart
+++ lib/mixin/mutexmixin.dart
@@ -3,7 +3,5 @@ import 'package:mutex/mutex.dart';
 mixin MutexMixin {
   final Mutex _mutex = Mutex();
 
-  Future<T> safe<T>(Future<T> Function() action) {
-    return _mutex.protect(action);
-  }
+  Future<T> safe<T>(Future<T> Function() action) => _mutex.protect(action);
 }
