6d4dfc54334b67f3e99de1116adaa8deb9f6c460
Nowaha <nowaha@proton.me>
2025-06-19 14:46:00 +0200

    Change ViewPost to use DataHolder

--- lib/screens/post/viewpost.dart
+++ lib/screens/post/viewpost.dart
@@ -1,7 +1,7 @@
 import 'package:assignment_sem6/data/service/data/postview.dart';
 import 'package:assignment_sem6/data/service/postservice.dart';
-import 'package:assignment_sem6/mixin/toastmixin.dart';
 import 'package:assignment_sem6/widgets/comment/commentsection.dart';
+import 'package:assignment_sem6/widgets/dataholderstate.dart';
 import 'package:assignment_sem6/widgets/screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -15,79 +15,34 @@ class ViewPost extends StatefulWidget {
   State<StatefulWidget> createState() => _ViewPostState();
 }
 
-class _ViewPostState extends State<ViewPost> with ToastMixin {
-  bool _isLoading = true;
-  PostView? post;
-
-  void fetchPost() async {
-    _isLoading = true;
-    setState(() {});
-
-    final postService = context.read<PostService>();
-
-    await Future.delayed(const Duration(milliseconds: 2500));
-
-    try {
-      post = await postService.getByUUIDLinked(widget.postUUID);
-      if (mounted) {
-        setState(() {
-          _isLoading = false;
-        });
-      }
-    } catch (error) {
-      print(error);
-
-      if (mounted) {
-        showToast("Post failed to load.");
-        setState(() {
-          _isLoading = false;
-        });
-      }
-    }
-  }
-
+class _ViewPostState extends DataHolderState<ViewPost, PostView> {
   @override
-  void initState() {
-    super.initState();
-
-    fetchPost();
-  }
-
-  Widget _loadingIndicator() => Column(
+  Widget content(BuildContext context) => Column(
+    mainAxisAlignment: MainAxisAlignment.start,
+    crossAxisAlignment: CrossAxisAlignment.start,
     spacing: 16,
-    children: [Text("Fetching post..."), const CircularProgressIndicator()],
+    children: [
+      Text("hi :3"),
+      Text(
+        "Author: ${data?.creator?.firstName ?? "Unknown"} ${data?.creator?.lastName ?? ""}",
+      ),
+      CommentSection(comments: data?.comments?.values.toList() ?? []),
+    ],
   );
 
-  Widget _errorIndicator() => Text("Failed to fetch post.");
+  @override
+  Future<PostView?> getDataFromSource() async {
+    final postService = context.read<PostService>();
+    return await postService.getByUUIDLinked(widget.postUUID);
+  }
 
   @override
-  Widget build(BuildContext context) {
-    final Widget child;
-
-    if (_isLoading) {
-      child = _loadingIndicator();
-    } else if (post == null) {
-      child = _errorIndicator();
-    } else {
-      child = Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        spacing: 16,
-        children: [
-          Text("hi :3"),
-          Text(
-            "Author: ${post?.creator?.firstName ?? "Unknown"} ${post?.creator?.lastName ?? ""}",
-          ),
-          CommentSection(comments: post?.comments?.values.toList() ?? []),
-        ],
-      );
-    }
-
-    return Screen.scroll(
-      title: Text(post?.post.title ?? "Loading..."),
-      alignment:
-          _isLoading || post == null ? Alignment.center : Alignment.topLeft,
-      child: child,
-    );
-  }
+  Widget build(BuildContext context) => Screen.scroll(
+    title: Text(data?.post.title ?? "Loading..."),
+    alignment:
+        loadingState == LoadingState.success
+            ? Alignment.topLeft
+            : Alignment.center,
+    child: getChild(context),
+  );
 }
