f7cd07217240deccb540b19c716925cc66b58748
Nowaha <nowaha@proton.me>
2025-06-30 02:29:50 +0200

    Add checkboxes to search filter

--- lib/widgets/view/filter/search.dart
+++ lib/widgets/view/filter/search.dart
@@ -13,28 +13,46 @@ class _SearchWidgetState extends State<SearchWidget> {
   final TextEditingController _searchController = TextEditingController();
 
   @override
-  Widget build(BuildContext context) => Row(
+  Widget build(BuildContext context) => Column(
     spacing: 8.0,
     children: [
-      Expanded(
-        child: ConstrainedBox(
-          constraints: const BoxConstraints(maxWidth: 400),
-          child: TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: "Search",
+      Row(
+        spacing: 8.0,
+        children: [
+          Expanded(
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(maxWidth: 400),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Search",
+                ),
+                controller: _searchController,
+                textInputAction: TextInputAction.search,
+                onSubmitted:
+                    (value) => widget.onSearch(value.trim().toLowerCase()),
+              ),
             ),
-            controller: _searchController,
-            textInputAction: TextInputAction.search,
-            onSubmitted: (value) => widget.onSearch(value.trim().toLowerCase()),
           ),
-        ),
+          IconButton.filledTonal(
+            onPressed: () {
+              widget.onSearch(_searchController.text.trim().toLowerCase());
+            },
+            icon: const Icon(Icons.search),
+          ),
+        ],
       ),
-      IconButton.filledTonal(
-        onPressed: () {
-          widget.onSearch(_searchController.text.trim().toLowerCase());
-        },
-        icon: const Icon(Icons.search),
+      Row(
+        children: [
+          Checkbox(value: true, onChanged: (value) {}),
+          const Text("Include Title"),
+          const SizedBox(width: 8.0),
+          Checkbox(value: true, onChanged: (value) {}),
+          const Text("Include Contents"),
+          const SizedBox(width: 8.0),
+          Checkbox(value: true, onChanged: (value) {}),
+          const Text("Include Tags"),
+        ],
       ),
     ],
   );
