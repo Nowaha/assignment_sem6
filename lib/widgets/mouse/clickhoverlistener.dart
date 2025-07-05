58a75ec334e60e488a68931b6a62076a5384e71a
Nowaha <nowaha@proton.me>
2025-07-06 01:51:58 +0200

    Add mouse cursor to ClickHoverListener

--- lib/widgets/mouse/clickhoverlistener.dart
+++ lib/widgets/mouse/clickhoverlistener.dart
@@ -4,17 +4,19 @@ import 'package:flutter/widgets.dart';
 /// A wrapper for [ClickListener] that also listens for mouse hover events.
 /// See [ClickListener] for more details as to why this is necessary.
 class ClickHoverListener extends StatelessWidget {
-  final Widget child;
   final VoidCallback? onClick;
   final VoidCallback? mouseEnter;
   final VoidCallback? mouseLeave;
+  final MouseCursor? cursor;
+  final Widget child;
 
   const ClickHoverListener({
     super.key,
-    required this.child,
     this.onClick,
     this.mouseEnter,
     this.mouseLeave,
+    this.cursor,
+    required this.child,
   });
 
   @override
@@ -22,6 +24,7 @@ class ClickHoverListener extends StatelessWidget {
     return MouseRegion(
       onEnter: (_) => mouseEnter?.call(),
       onExit: (_) => mouseLeave?.call(),
+      cursor: cursor ?? SystemMouseCursors.click,
       child: ClickListener(onClick: onClick ?? () {}, child: child),
     );
   }
